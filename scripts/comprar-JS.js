c0cb8fb2c294074f57e8e8fb21ae0dbebbb5f9d2
EnzoSart <oscuridadluminosa@gmail.com>
2018-11-10 14:11:54 -0300

    Now it disappears when i check it

--- scripts/comprar-JS.js
+++ scripts/comprar-JS.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var campoCantidad = document.getElementById("quantity");
     var precioTotal = document.getElementById("totalAmount");
     var CD = document.getElementById("cd");
-    var digital = document.getElementById("digital");
+    var digital = document.getElementById("digit");
     var shpmethod = document.getElementById("shp-method-sel");
 
     campoCantidad.addEventListener("change", function(){
@@ -13,21 +13,16 @@ document.addEventListener("DOMContentLoaded", function(){
 
     CD.addEventListener("change", function(){
         if (CD.checked) {
-            document.getElementById("address-div").style.display = "block";
-            document.getElementById("shp-method-div").style.display = "block";
-        }
-    });
-
-    digital.addEventListener("change", function(){
-        if (digital.checked) {
             document.getElementById("address-div").style.display = "none";
             document.getElementById("shp-method-div").style.display = "none";
         }
     });
 
-
-
-
-
+    digital.addEventListener("change", function (){
+        if (digital.checked) {
+            document.getElementById("address-div").style.display = "block";
+            document.getElementById("shp-method-div").style.display = "block";
+        }
+    })
 
 });
