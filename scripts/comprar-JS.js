e8a931b778be977fbba70c44bbe69ef21e4d092e
EnzoSart <oscuridadluminosa@gmail.com>
2018-11-13 20:25:56 -0300

    SHIPPPING

--- scripts/comprar-JS.js
+++ scripts/comprar-JS.js
@@ -4,11 +4,13 @@ document.addEventListener("DOMContentLoaded", function(){
     var precioTotal = document.getElementById("totalAmount");
     var CD = document.getElementById("cd");
     var digital = document.getElementById("digit");
-    var shpmethod = document.getElementById("shp-method-sel");
+    var campoEnvio = document.getElementById("shp-method-sel");
+    var precioActual;
 
     campoCantidad.addEventListener("change", function(){
         var cantidad = parseInt(campoCantidad.value);
-        precioTotal.innerHTML = "$" + cantidad * precio;
+        precioActual = cantidad * precio;
+        precioTotal.innerHTML = "$" + precioActual;
     });
 
     CD.addEventListener("change", function(){
@@ -24,5 +26,27 @@ document.addEventListener("DOMContentLoaded", function(){
             document.getElementById("shp-method-div").style.display = "none";
         }
     })
+
+    campoEnvio.addEventListener("change", function(){
+        var metodo = campoEnvio.value;
+        var costoTotal;
+        if (metodo=="normal"){
+            costoTotal = precioActual * 1.005;
+            precioTotal.innerHTML = "$" + costoTotal;
+        }
+        else if (metodo=="special"){
+            costoTotal = precioActual * 1.02;
+            precioTotal.innerHTML = "$" + costoTotal;
+        }
+        else if (metodo=="premium"){
+            costoTotal = precioActual * 1.05;
+            precioTotal.innerHTML = "$" + costoTotal;
+        }
+        else {
+            precioTotal.innerHTML = "$" + precioActual;
+        }
+    })
+
+
 
 });
