bd3e14935446d8a3c0bb4ce05ad87dd4f2dbd5e6
EnzoSart <oscuridadluminosa@gmail.com>
2018-11-20 20:04:58 -0300

    HOLA MI AMOR COMO ESTAS

--- scripts/comprar-JS.js
+++ scripts/comprar-JS.js
@@ -5,18 +5,41 @@ document.addEventListener("DOMContentLoaded", function(){
     var CD = document.getElementById("cd");
     var digital = document.getElementById("digit");
     var campoEnvio = document.getElementById("shp-method-sel");
+    document.getElementById("address-div").style.display = "none";
+    document.getElementById("shp-method-div").style.display = "none";
     var precioActual;
+    precioTotal.innerHTML = precio;
+
 
     campoCantidad.addEventListener("change", function(){
         var cantidad = parseInt(campoCantidad.value);
         precioActual = cantidad * precio;
-        precioTotal.innerHTML = "$" + precioActual;
+        precioTotal.innerHTML = "$" + precioActual.toFixed(2);
     });
 
     CD.addEventListener("change", function(){
         if (CD.checked) {
             document.getElementById("address-div").style.display = "block";
             document.getElementById("shp-method-div").style.display = "block";
+            var metodo = campoEnvio.value;
+            var costoTotal;
+            if (metodo=="normal"){
+                costoTotal = precioActual * 1.005;
+                precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
+            }
+            else if (metodo=="special"){
+                costoTotal = precioActual * 1.02;
+                precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
+            }
+            else if (metodo=="premium"){
+                costoTotal = precioActual * 1.05;
+                precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
+            }
+            else {
+                precioTotal.innerHTML = "$" + precioActual.toFixed(2);
+            }
+        
+
         }
     });
 
@@ -24,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function(){
         if (digital.checked) {
             document.getElementById("address-div").style.display = "none";
             document.getElementById("shp-method-div").style.display = "none";
+            precioTotal.innerHTML = "$" + precioActual.toFixed(2);
         }
     })
 
@@ -32,20 +56,22 @@ document.addEventListener("DOMContentLoaded", function(){
         var costoTotal;
         if (metodo=="normal"){
             costoTotal = precioActual * 1.005;
-            precioTotal.innerHTML = "$" + costoTotal;
+            precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
         }
         else if (metodo=="special"){
             costoTotal = precioActual * 1.02;
-            precioTotal.innerHTML = "$" + costoTotal;
+            precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
         }
         else if (metodo=="premium"){
             costoTotal = precioActual * 1.05;
-            precioTotal.innerHTML = "$" + costoTotal;
+            precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
         }
         else {
-            precioTotal.innerHTML = "$" + precioActual;
+            precioTotal.innerHTML = "$" + precioActual.toFixed(2);
         }
     })
+
+
 
 
 
